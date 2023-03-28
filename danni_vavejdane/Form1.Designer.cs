2651c82057452aaa6d765fa0dcadfac6138cca2a
MHristozova1 <114220348+MHristozova1@users.noreply.github.com>
2023-03-28 09:12:42 +0300

    Add files via upload

---
+++ danni_vavejdane/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace danni_vavejdane
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.запазванеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.новToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.запазванеToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.изходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.firstName = new System.Windows.Forms.TextBox();
+            this.lastName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.maleButton = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.titleBox = new System.Windows.Forms.ComboBox();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.запазванеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(325, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // запазванеToolStripMenuItem
+            // 
+            this.запазванеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.новToolStripMenuItem,
+            this.запазванеToolStripMenuItem1,
+            this.изходToolStripMenuItem});
+            this.запазванеToolStripMenuItem.Name = "запазванеToolStripMenuItem";
+            this.запазванеToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.запазванеToolStripMenuItem.Text = "Файл";
+            this.запазванеToolStripMenuItem.Click += new System.EventHandler(this.запазванеToolStripMenuItem_Click);
+            // 
+            // новToolStripMenuItem
+            // 
+            this.новToolStripMenuItem.Name = "новToolStripMenuItem";
+            this.новToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.новToolStripMenuItem.Text = "Нов";
+            // 
+            // запазванеToolStripMenuItem1
+            // 
+            this.запазванеToolStripMenuItem1.Name = "запазванеToolStripMenuItem1";
+            this.запазванеToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.запазванеToolStripMenuItem1.Text = "Запазване";
+            // 
+            // изходToolStripMenuItem
+            // 
+            this.изходToolStripMenuItem.Name = "изходToolStripMenuItem";
+            this.изходToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.изходToolStripMenuItem.Text = "Изход";
+            this.изходToolStripMenuItem.Click += new System.EventHandler(this.изходToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Название";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Име";
+            // 
+            // firstName
+            // 
+            this.firstName.Location = new System.Drawing.Point(83, 69);
+            this.firstName.Name = "firstName";
+            this.firstName.Size = new System.Drawing.Size(153, 20);
+            this.firstName.TabIndex = 3;
+            this.firstName.TextChanged += new System.EventHandler(this.firstName_TextChanged);
+            // 
+            // lastName
+            // 
+            this.lastName.Location = new System.Drawing.Point(97, 99);
+            this.lastName.Name = "lastName";
+            this.lastName.Size = new System.Drawing.Size(139, 20);
+            this.lastName.TabIndex = 4;
+            this.lastName.TextChanged += new System.EventHandler(this.lastName_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Фамилия";
+            // 
+            // maleButton
+            // 
+            this.maleButton.AutoSize = true;
+            this.maleButton.Location = new System.Drawing.Point(17, 19);
+            this.maleButton.Name = "maleButton";
+            this.maleButton.Size = new System.Drawing.Size(48, 17);
+            this.maleButton.TabIndex = 6;
+            this.maleButton.TabStop = true;
+            this.maleButton.Text = "мъж";
+            this.maleButton.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(17, 51);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(51, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "жена";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.groupBox1.Controls.Add(this.maleButton);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(38, 157);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(116, 90);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Пол";
+            // 
+            // titleBox
+            // 
+            this.titleBox.FormattingEnabled = true;
+            this.titleBox.Items.AddRange(new object[] {
+            "госпожа",
+            "господин",
+            "госпожица",
+            "дете"});
+            this.titleBox.Location = new System.Drawing.Point(97, 42);
+            this.titleBox.Name = "titleBox";
+            this.titleBox.Size = new System.Drawing.Size(139, 21);
+            this.titleBox.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 450);
+            this.Controls.Add(this.titleBox);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lastName);
+            this.Controls.Add(this.firstName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Данни въвеждане";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem запазванеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem новToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem запазванеToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem изходToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox firstName;
+        private System.Windows.Forms.TextBox lastName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton maleButton;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox titleBox;
+    }
+}
+
---
+++ danni_vavejdane/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace danni_vavejdane
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void запазванеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void изходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void firstName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lastName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
